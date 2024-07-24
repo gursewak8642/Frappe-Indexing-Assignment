6f4cd37fee4fffb795d38c23392aa462433f8805
gursewak8642 <gursewak8642@gmail.com>
2024-07-24 12:52:15 +0530

    Assignment 4

--- serverscript/serverscript/doctype/myscheduler/myscheduler.js
+++ serverscript/serverscript/doctype/myscheduler/myscheduler.js
@@ -1,8 +1,57 @@
 // Copyright (c) 2024, Gursewak Singh and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("myScheduler", {
-// 	refresh(frm) {
+    // Copyright (c) 2024, Gursewak Singh and contributors
+    // For license information, please see license.txt
 
-// 	},
-// });
+    frappe.ui.form.on("myScheduler", {
+    	refresh(frm) {
+
+            let d = new frappe.ui.Dialog({
+                title: 'Enter details',
+                fields: [
+                    {
+                        label: 'First Name',
+                        fieldname: 'first_name',
+                        fieldtype: 'Link',
+                        options: 'Address'
+                    }
+                ],
+                size: 'small', // small, large, extra-large 
+                primary_action_label: 'Submit',
+                primary_action(values) {
+                    frm.set_value("first_name",values.first_name)
+                    d.hide();
+                }
+            });
+            d.show();
+
+    	},
+    });
+
+
+
+
+
+    // frappe.ui.form.on("ErrorLog", {
+    //     refresh(frm) {
+    //         let d = new frappe.ui.Dialog({
+    //             fields: [
+                    // {
+                    //     label: 'First Name',
+                    //     fieldname: 'first_name',
+                    //     fieldtype: 'Link',
+                    //     options: 'Address'
+                    // }
+    //             ],
+    //             size: 'small',
+    //             primary_action_label: 'Submit',
+    //             primary_action(values) {
+    //                 console.log("values",values);
+    //                 frm.set_value("first_name",values.first_name)
+    //                 d.hide();
+    //             }
+    //         });
+    //         d.show();
+    //     },
+    // });
